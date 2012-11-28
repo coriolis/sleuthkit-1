ae7b4a030da6b05d85e73fa0003d70a90354c275
Rohan <rohan.nandode@coriolis.co.in>
2012-11-28 19:39:45 +0530

    - setup printf to send message to log

--- tools/fstools/slt-pre.js
+++ tools/fstools/slt-pre.js
@@ -1,4 +1,19 @@
-var arguments = [ "/home2/rohan/work/tmp/testdisk1.vmdk"];
+
+// Our generated code 
+importScripts('myrt.js');
+self.onmessage = function(ev) {
+    setup_file(ev.data['fs']);
+    Module['run']([ev.data['fn']]);
+
+};
+
 
 var Module= { 'noInitialRun': true} ;
+//set print to be postMessage
+Module['print'] = function (x) {
+   self.postMessage(x);
+};
 
+
+
+// our code end 
