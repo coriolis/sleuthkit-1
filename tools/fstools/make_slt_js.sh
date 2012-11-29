5044fd9e650eec57cfe53958f618214ae480d1f7
Rohan <rohan.nandode@coriolis.co.in>
2012-11-29 14:58:50 +0530

    - Link qemu lib statically

--- tools/fstools/make_slt_js.sh
+++ tools/fstools/make_slt_js.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-emcc  slt.o ../../tsk3/.libs/libtsk3.a -o slt.js --pre-js slt-pre.js -s RUNTIME_LINKED_LIBS="['qemu-img-lib.js']" 
+if [ ! -f ./qemu-img-lib.bc ] ; then
+    echo "Error: file  qemu-img-lib.bc not found."
+    echo "Copy qemu-img-lib.bc file from QEMU source directory in this folder, generated after compiling QEMU."
+    exit 0
+fi
+
+emcc  slt.o ../../tsk3/.libs/libtsk3.a ./qemu-img-lib.bc -o slt.js --pre-js slt-pre.js 
+#-s RUNTIME_LINKED_LIBS="['qemu-img-lib.js']" 
