016ad04d64d2562652651d6a3f7598d59d2b6692
Rohan <rohan.nandode@coriolis.co.in>
2012-12-06 12:29:40 +0530

    - print is now logger with postmessage - cmd args are array so no need to parse

--- tools/fstools/slt-pre.js
+++ tools/fstools/slt-pre.js
@@ -2,20 +2,35 @@
 // Our generated code 
 importScripts('myrt.js');
 self.onmessage = function(ev) {
-    setup_file(ev.data['fs']);
-    Module['run']([ev.data['fn']]);
+
+    switch(ev.data['type']) {
+        case 'init': 
+            setup_file(ev.data.data);
+            break
+        case 'send':
+            var s = ev.data.data; 
+            s[s.length-1] = '/' + fileInstance.name;
+            try {
+                Module['run'](s);
+            } 
+            catch(e) {
+                //pass
+            }
+
+            break;
+    }
+    //Module['run']([ev.data.data]);
 
 };
 
 
-var stdoutcache = '', stderrcache = '';
+var stdincache = Array(), stdoutcache = '', stderrcache = '';
 function generic_output(type, buffer, x) {
+    buffer += String.fromCharCode(x);
     if(x == 10) {
         self.postMessage({'type': type, 'text': buffer});
         buffer = '';
     }
-    else
-        buffer += String.fromCharCode(x);
     return buffer;
 }
 
@@ -26,17 +41,22 @@ function mystderr(x) {
     stderrcache = generic_output(2, stderrcache, x);
 }
 
+function send_log_message(x) {
+        self.postMessage({'type': 2, 'text': x});
+}
+
 function mystdin() {
-    return null;
+    line = stdincache.shift();
+    return line;
 }
 
 function vmx_do_init() {
     FS.init(mystdin, mystdout, mystderr);
 }
 
-var Module= { 'noInitialRun': true} ;
+var Module= { 'noInitialRun': true, 'noExitRuntime': true} ;
 //set print to be postMessage
-Module['print'] = mystdout;
+Module['print'] = send_log_message;
 
 Module['preInit'] = vmx_do_init;
 
