a3b1ebf543e337eb0e25483aa00af2abed34142a
Rohan <rohan.nandode@coriolis.co.in>
2013-08-06 20:43:13 +0530

    - Addind libosinfo.bc

--- tools/fstools/make_slt_js.sh
+++ tools/fstools/make_slt_js.sh
@@ -5,7 +5,14 @@ if [ ! -f ./qemu-img-lib.bc ] ; then
     exit 0
 fi
 
-emcc  slt.o ../../tsk3/.libs/libtsk3.a ./qemu-img-lib.bc -O1 -o slt.js --pre-js slt-pre.js --js-library slt-lib.js
+LIB_OS_INFO="./libosinfo.bc"
+if [ ! -f $LIB_OS_INFO ] ; then
+    echo "Warning: file libosinfo.bc not found. OSInfo button will not work"
+    echo "Copy libosinfo.bc file from vminspection ource directory in this folder, generated after compiling vminspection."
+    LIB_OS_INFO=""
+fi
+
+emcc  slt.o ../../tsk3/.libs/libtsk3.a $LIB_OS_INFO ./qemu-img-lib.bc -O1 -o slt.js --pre-js slt-pre.js --js-library slt-lib.js
 
 #-s EXPORTED_FUNCTIONS=['_pread'] 
 #-s RUNTIME_LINKED_LIBS="['qemu-img-lib.js']" 
